983c9003e385883b5bc3fc9fec8e069246be14e1
FordangIT <fordang0819@gmail.com>
2024-01-06 21:56:22 +0900

    feat: preview 페이지

---
+++ src/pages/Preview.tsx
@@ -0,0 +1,12 @@
+import Title from "../components/Preview/Title";
+const Preview = () => {
+  return (
+    <div className="w-screen min-h-screen bg-light-purple ">
+      <div className="position">
+        <Title />
+      </div>
+      {/* <Question /> */}
+    </div>
+  );
+};
+export default Preview;
