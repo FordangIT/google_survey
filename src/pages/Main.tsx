c1b370a4941db69a45bead4b263202cce6debfb3
FordangIT <fordang0819@gmail.com>
2024-01-06 21:53:22 +0900

    feat: 메인 구조 변경

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,9 +3,13 @@ import Question from "../components/Main/Question";
 import AddPreview from "../components/Main/AddPreview";
 const Main = () => {
   return (
-    <div className="w-screen h-100% bg-light-purple ">
-      <Title />
-      <AddPreview />
+    <div className="w-screen min-h-screen bg-light-purple ">
+      <div className="position">
+        <Title />
+        <div className="absolute top-12 right-[20.5rem]">
+          <AddPreview />
+        </div>
+      </div>
       <Question />
     </div>
   );
